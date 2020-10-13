cb250b72f7930bb1fc4ad8d85c4ce781e7911117
Anil <anil@Meghanas-MBP.fios-router.home>
2020-10-12 23:54:42 -0400

    update

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  future: {},
+  purge: [],
+  theme: {
+    fontFamily: {
+      serif: ["Roboto"],
+    },
+    extend: {
+      width: {
+        72: "18rem",
+      },
+      height: {
+        72: "18rem",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
