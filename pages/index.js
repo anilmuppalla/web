a277e7a18bfefd9e9dc846c704ffdf24fd26e2ed
Anil <anil@Meghanas-MBP.fios-router.home>
2020-11-07 21:21:27 -0500

    update tag css and code structure

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import Hero from "./components/hero";
 function Home(props) {
   const [showPage, setPage] = useState(props.showPage);
 
@@ -16,85 +16,15 @@ function Home(props) {
       </head>
       <body>
         <div className="p-4 mx-auto max-w-screen-md">
-          <div className="flex space-x-48 justify-between items-center">
-            <div>
-              <h1 className="font-mono text-5xl text-gray-800">
-                Anil Muppalla
-              </h1>
-              <h1 className="font-mono text-2xl text-gray-600">
-                Software Engineer in New York
-              </h1>
-              <div className="mt-8 space-x-4">
-                <a
-                  className="underline text-blue-600 text-lg"
-                  target="_blank"
-                  rel="noopener"
-                  href="https://twitter.com/anilmuppallar"
-                >
-                  twitter
-                </a>
-                <a
-                  className="underline text-purple-600 text-lg"
-                  target="_blank"
-                  rel="noopener"
-                  href="https://www.linkedin.com/in/anilmuppalla/"
-                >
-                  linkedin
-                </a>
-                <a
-                  className="underline text-red-600 text-lg"
-                  target="_blank"
-                  rel="noopener"
-                  href="https://github.com/anilmuppalla"
-                >
-                  github
-                </a>
-              </div>
-            </div>
-            <div>
-              <img
-                className="w-56 h-56 rounded-full object-cover object-right-bottom"
-                src="/img/anil_2.jpg"
-                alt="anil muppalla"
-              />
-            </div>
-          </div>
-          <div className="mt-6">
-            <div className="text-lg">
-              <p>
-                Hello there 👋 ! My name is Anil and I am a Senior Engineer at
-                <a
-                  className="text-green-600 font-bold no-underline hover:underline"
-                  rel="noopener"
-                  target="_blank"
-                  href="https://spotify.com/us"
-                >
-                  {" "}
-                  Spotify.
-                </a>
-              </p>
-              <p className="mt-4">
-                I am currently working on building large scale distributed data
-                systems. In the past I have co-founded a SAAS startup that
-                provided analytical tooling for educational institutions.
-              </p>
-              <p className="mt-4">
-                I am also very fond of web and mobile technologies and I tend to
-                spend a significant portion of my free time exploring them.
-              </p>
-              <p className="mt-4">
-                Drop me a message if you want to connect/chat.
-              </p>
-            </div>
-          </div>
-          <div className="mt-4 space-x-8">
+          <Hero />
+          <div className="mt-4 space-x-4">
             <button
               onClick={() => setPage("speaker")}
               className={` ${
                 showPage === "speaker"
-                  ? "bg-teal-400 text-white border-transparent"
-                  : " hover:bg-teal-400 text-blue-700 hover:text-white bg-transparent hover:border-transparent border-blue-500"
-              }   font-semibold  py-2 px-4 border rounded`}
+                  ? "bg-teal-400 text-white border-transparent focus:outline-none"
+                  : " hover:bg-teal-400 text-blue-700 hover:text-white bg-transparent hover:border-transparent focus:outline-none border-blue-500"
+              }   font-semibold  py-2 px-4 border rounded-full`}
             >
               Speaker
             </button>
@@ -102,9 +32,9 @@ function Home(props) {
               onClick={() => setPage("blog")}
               className={` ${
                 showPage === "blog"
-                  ? "bg-teal-400 text-white border-transparent"
-                  : " hover:bg-teal-400 text-blue-700 hover:text-white bg-transparent hover:border-transparent border-blue-500"
-              }   font-semibold  py-2 px-4 border rounded`}
+                  ? "bg-teal-400 text-white border-transparent active:outline-none focus:outline-none"
+                  : " hover:bg-teal-400 text-blue-700 hover:text-white bg-transparent hover:border-transparent focus:outline-none border-blue-500"
+              }   font-semibold  py-2 px-4 border rounded-full`}
             >
               Blog
             </button>
@@ -112,9 +42,9 @@ function Home(props) {
               onClick={() => setPage("book")}
               className={` ${
                 showPage === "book"
-                  ? "bg-teal-400 text-white border-transparent"
-                  : " hover:bg-teal-400 text-blue-700 hover:text-white bg-transparent hover:border-transparent border-blue-500"
-              }   font-semibold  py-2 px-4 border rounded`}
+                  ? "bg-teal-400 text-white border-transparent focus:outline-none"
+                  : " hover:bg-teal-400 text-blue-700 hover:text-white bg-transparent hover:border-transparent focus:outline-none border-blue-500"
+              }   font-semibold  py-2 px-4 border rounded-full`}
             >
               Book
             </button>
